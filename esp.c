6bdc591149fc36c3ea18284904e8bbfeef128958
Luke Macenski <57783708+lmacenski@users.noreply.github.com>
2024-02-25 02:23:46 -0500

    Include Stepper Code

--- esp.c
+++ esp.c
@@ -6,10 +6,22 @@
 #include <WiFiClient.h>
 #include <WiFiClientSecure.h>
 #include <WiFiClientSecureBearSSL.h>
+#include <AccelStepper.h>
 
 //Setup Wifi and HTTPS
 BearSSL::WiFiClientSecure httpsClient;
 const char *ssid = "1819_Guest";
+
+//Define Stepper Pins
+#define IN1 5
+#define IN2 4
+#define IN3 14
+#define IN4 12
+
+//Declare Stepper Vars/Library
+bool a = false; 
+const int end = 1024;
+AccelStepper stepper1(4, IN1, IN3, IN2, IN4);
 
 //Define HTTPS Request String
 String payload;
@@ -28,10 +40,15 @@ String findText(String input, String keyword, int read) {
   return input.substring(start, start + read);
 }
 
-
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
+
+
+  //Setup Stepper 1 Attributes
+  stepper1.setMaxSpeed(3500.0);
+  stepper1.setAcceleration(100.0);
+  stepper1.setSpeed(3500);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid);
@@ -46,6 +63,20 @@ void setup() {
   // Set offset time in seconds to adjust for your timezone, for example:
   // GMT +1 = 3600
   timeClient.setTimeOffset(-18000);
+}
+
+//Rotate Stepper 1
+void rotate(int n) {
+  for(int i =0; i<n;i++)
+  {
+  stepper1.moveTo(end);
+  //Serial.println(stepper1.currentPosition());
+  if(stepper1.currentPosition()==end)
+  {
+  stepper1.setCurrentPosition(0);
+  a=false;
+  }
+  }
 }
 
 void loop() {
@@ -68,10 +99,9 @@ void loop() {
     int httpCode = https.GET();                                       
     if (httpCode > 0) { 
       payload = https.getString();
-      
-
     }
-    https.end();  
+    https.end(); 
+
   //Declare Strings to Be Found in the Json Payload
   String ft0 = "Time_0\":{\"type\":\"TIME\",\"value\":\"";
   String ft1 = "Time_1\":{\"type\":\"TIME\",\"value\":\"";
@@ -84,11 +114,29 @@ void loop() {
   String t2 = findText(payload, ft2, 5);
   String nump = findText (payload, fnump, 1);
 
-  
+  //Print all Values to Serial Console for Debugging
   Serial.println(t0);
   Serial.println(t1);
   Serial.println(t2);
   Serial.println(nump);
   Serial.println("");
+  
+  //Determine if medicine must dispense
+  if(formaltime.equals(t0) || t1.equals(t1) || formaltime.equals(t2)){
+    for (int i = 1; i <= nump.toInt(); i++){
+      stepper1.setMaxSpeed(3500.0);
+      stepper1.setAcceleration(100.0);
+      stepper1.setSpeed(3500);
+      rotate(1);
+    }
+  }
+  else{
+    stepper1.setMaxSpeed(0);
+    stepper1.setAcceleration(0);
+    stepper1.setSpeed(0);
+    return;
+  }
+
+  //Set Delay before Looping Again
   delay(5000);
 }
